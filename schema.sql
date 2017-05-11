66a1a3b85533fed8a18fa96a3e3668274f62cc3d
Mark Fisher <markisaacfisher@gmail.com>
2017-05-12 00:51:10 +1000

    Add database schema

---
+++ schema.sql
@@ -0,0 +1,229 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 11, 2017 at 11:10 AM
+-- Server version: 5.6.28
+-- PHP Version: 5.5.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `votely`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ballots`
+--
+
+CREATE TABLE `ballots` (
+  `id` int(10) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `organisation` varchar(100) NOT NULL,
+  `state` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `candidates`
+--
+
+CREATE TABLE `candidates` (
+  `id` int(10) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `ballot_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `organisations`
+--
+
+CREATE TABLE `organisations` (
+  `organisation_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `preference`
+--
+
+CREATE TABLE `preference` (
+  `vote_id` int(10) NOT NULL,
+  `candidate_id` int(10) NOT NULL,
+  `preference` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roll_call`
+--
+
+CREATE TABLE `roll_call` (
+  `user_id` int(10) NOT NULL,
+  `ballot_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(8) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(1023) NOT NULL,
+  `organisation` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vote`
+--
+
+CREATE TABLE `vote` (
+  `id` int(10) NOT NULL,
+  `ballot_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ballots`
+--
+ALTER TABLE `ballots`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `organisation` (`organisation`);
+
+--
+-- Indexes for table `candidates`
+--
+ALTER TABLE `candidates`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ballot_id` (`ballot_id`);
+
+--
+-- Indexes for table `organisations`
+--
+ALTER TABLE `organisations`
+  ADD PRIMARY KEY (`organisation_name`);
+
+--
+-- Indexes for table `preference`
+--
+ALTER TABLE `preference`
+  ADD PRIMARY KEY (`vote_id`,`candidate_id`),
+  ADD KEY `candidate_id` (`candidate_id`);
+
+--
+-- Indexes for table `roll_call`
+--
+ALTER TABLE `roll_call`
+  ADD PRIMARY KEY (`user_id`,`ballot_id`),
+  ADD KEY `ballot_id` (`ballot_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users` (`organisation`,`username`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `organisation` (`organisation`);
+
+--
+-- Indexes for table `vote`
+--
+ALTER TABLE `vote`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ballot_id` (`ballot_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ballots`
+--
+ALTER TABLE `ballots`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+--
+-- AUTO_INCREMENT for table `candidates`
+--
+ALTER TABLE `candidates`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `vote`
+--
+ALTER TABLE `vote`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `candidates`
+--
+ALTER TABLE `candidates`
+  ADD CONSTRAINT `candidates_ibfk_1` FOREIGN KEY (`ballot_id`) REFERENCES `ballots` (`id`);
+
+--
+-- Constraints for table `organisations`
+--
+ALTER TABLE `organisations`
+  ADD CONSTRAINT `organisations_ibfk_1` FOREIGN KEY (`organisation_name`) REFERENCES `ballots` (`organisation`);
+
+--
+-- Constraints for table `preference`
+--
+ALTER TABLE `preference`
+  ADD CONSTRAINT `preference_ibfk_1` FOREIGN KEY (`vote_id`) REFERENCES `vote` (`id`),
+  ADD CONSTRAINT `preference_ibfk_2` FOREIGN KEY (`candidate_id`) REFERENCES `candidates` (`id`);
+
+--
+-- Constraints for table `roll_call`
+--
+ALTER TABLE `roll_call`
+  ADD CONSTRAINT `roll_call_ibfk_1` FOREIGN KEY (`ballot_id`) REFERENCES `ballots` (`id`),
+  ADD CONSTRAINT `roll_call_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`organisation`) REFERENCES `organisations` (`organisation_name`);
+
+--
+-- Constraints for table `vote`
+--
+ALTER TABLE `vote`
+  ADD CONSTRAINT `vote_ibfk_1` FOREIGN KEY (`ballot_id`) REFERENCES `ballots` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
